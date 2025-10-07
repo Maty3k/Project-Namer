889de5f644e1de1ca3788ea8152fcda4ef14100f
Ana Maria Radulescu <anamariaradulescu@Mati-mac.local>
2025-10-07 14:17:47 +0200

    Revert "Update appearance settings to use button style like theme customizer"
    This reverts commit a0e4f877469aee1ca5f654713f36fba65643cc40.

--- resources/views/livewire/settings/appearance.blade.php
+++ resources/views/livewire/settings/appearance.blade.php
@@ -2,33 +2,10 @@
     @include('partials.settings-heading')
 
     <x-settings.layout :heading="__('Appearance')" :subheading=" __('Update the appearance settings for your account')">
-        <div class="flex items-center space-x-2" x-data>
-            <flux:button
-                @click="$flux.appearance = 'light'"
-                ::variant="$flux.appearance === 'light' ? 'primary' : 'ghost'"
-                size="sm"
-                icon="sun"
-            >
-                {{ __('Light') }}
-            </flux:button>
-
-            <flux:button
-                @click="$flux.appearance = 'dark'"
-                ::variant="$flux.appearance === 'dark' ? 'primary' : 'ghost'"
-                size="sm"
-                icon="moon"
-            >
-                {{ __('Dark') }}
-            </flux:button>
-
-            <flux:button
-                @click="$flux.appearance = 'system'"
-                ::variant="$flux.appearance === 'system' ? 'primary' : 'ghost'"
-                size="sm"
-                icon="computer-desktop"
-            >
-                {{ __('System') }}
-            </flux:button>
-        </div>
+        <flux:radio.group x-data variant="segmented" x-model="$flux.appearance">
+            <flux:radio value="light" icon="sun">{{ __('Light') }}</flux:radio>
+            <flux:radio value="dark" icon="moon">{{ __('Dark') }}</flux:radio>
+            <flux:radio value="system" icon="computer-desktop">{{ __('System') }}</flux:radio>
+        </flux:radio.group>
     </x-settings.layout>
 </section>
