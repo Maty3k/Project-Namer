e13901be202889f1d9d04b6d65488de88b3c3f3b
Matei Darius <anamariaradulescu@Mati-mac.local>
2025-11-19 11:39:47 +0100

    Remove trust signals from hero section
    - Removed 'Always free', 'Quick signup', and 'Open source' checkmarks
    - Simplified hero section to focus on headline, description, and CTA

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -95,28 +95,6 @@
                         Get Started
                     </a>
                 </div>
-
-                {{-- Trust Signals --}}
-                <div class="flex flex-wrap items-center justify-center gap-8 pt-6 text-sm text-zinc-500 dark:text-zinc-400">
-                    <div class="flex items-center gap-2">
-                        <svg class="w-5 h-5 text-green-500" fill="currentColor" viewBox="0 0 20 20">
-                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"/>
-                        </svg>
-                        <span>Always free</span>
-                    </div>
-                    <div class="flex items-center gap-2">
-                        <svg class="w-5 h-5 text-green-500" fill="currentColor" viewBox="0 0 20 20">
-                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"/>
-                        </svg>
-                        <span>Quick signup</span>
-                    </div>
-                    <div class="flex items-center gap-2">
-                        <svg class="w-5 h-5 text-green-500" fill="currentColor" viewBox="0 0 20 20">
-                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"/>
-                        </svg>
-                        <span>Open source</span>
-                    </div>
-                </div>
             </div>
         </div>
     </section>
